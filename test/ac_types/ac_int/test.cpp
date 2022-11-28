ee5dbad2bf2db1274de381145398eea6b76cb7ce
zhan.zhang <zhan.zhang493@qq.com>
2022-11-28 20:23:47 +0800

    [test] revise ac_int test file

--- test/ac_types/ac_int/test.cpp
+++ test/ac_types/ac_int/test.cpp
@@ -2,14 +2,13 @@
 #include <stdio.h>
 #include "../../include/ac_types/ac_int.h"
 
-using std::cout;
-using std::endl;
-
-
+using namespace std;
 
 int main(){
 	typedef ac_int<5, true> int5;
 	int5 a = 3;
-	cout << a << endl;
+	int5 b = -13;
+	std::cout << a.to_string(AC_BIN, false, true) << std::endl;
+	std::cout << b.to_string(AC_BIN, false, true) << std::endl;
 	return 0;
 }
