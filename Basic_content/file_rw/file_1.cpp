0275e0b7a4315b384dbc7780b426be57e84871bb
zhan.zhang <zhan.zhang493@qq.com>
2023-02-19 22:20:05 +0800

    [file] file code

---
+++ Basic_content/file_rw/file_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* 文件操作 */
+// 通过文件可以将数据持久化
+// 文件类型分为两种：
+// 文本文件，文本的ASCII码形式进行存储
+// 二进制文件，二进制形式
+
+// 操作文件的三大类
+// ofstream: write
+// ifstream: read
+// fstream: read and write
+
+
+int main()
+{
+    return 0;
+}
